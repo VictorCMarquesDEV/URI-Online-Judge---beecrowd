3bc7018624d45a7f3649ac5497b6e831fffd1327
Victor Cavalcanti <113631460+VictorCMarquesDEV@users.noreply.github.com>
2022-10-23 10:27:15 -0300

    Add more files

---
+++ beecrowd in C++/1140 in C++.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void maiusculo(char s1[], char s2[]){
+    int i = 0;
+    while(s1[i] != '\0'){
+        s2[i] = toupper(s1[i]);
+        i++;
+        //printf("entrou maiusculo");
+    }
+    s2[i] = '\0';
+}
+
+int main(){
+
+	int i,i2,b,cont,v[1000],*p;
+	char s1[2001],s2[2001],aux[2];
+
+	do{
+		START:
+		//setbuf(stdin,NULL);
+		//printf("comecou\n");
+		scanf(" %[^\n]",s1);
+		//printf("Passou\n");
+		
+		if(s1[0]=='*'){
+			break;
+		}
+		
+		maiusculo(s1,s2);
+		aux[0]=s2[0];
+		
+		for(i=0,i2=0,cont=1;i<strlen(s2)-1;i++){
+			if((int)s2[i]==32){
+				cont++;
+				v[i2]=i;
+				i2++;
+			}
+		}
+		if(cont==1){
+			printf("Y\n");
+			goto START;
+		}
+		for(i=0,b=0,p=&v[0];i<cont-1;i++,p++){
+			if(s2[*p+1]==aux[0]){
+				b=1;
+			}else{
+				b=0;
+				break;
+		}
+	}
+		if(b==1){
+			printf("Y\n");
+		}else{
+			printf("N\n");
+		}
+	}while(1);
+	
+	return 0;
+}
---
+++ beecrowd in C++/2807 in C++.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int n, fibo[40], contador;
+    scanf("%i", &n);
+    
+    fibo[0] = 1;
+    fibo[1] = 1;
+    contador = 2;
+    
+    while (contador < n) {
+        fibo[contador] = fibo[contador-1] + fibo[contador-2];
+        contador++;
+    }
+    
+    if (n > 1) {
+        while (contador > 1) {
+            printf("%i ", fibo[--contador]);
+        }
+    }
+    printf("%i\n", fibo[0]);
+    
+    return 0;
+}
